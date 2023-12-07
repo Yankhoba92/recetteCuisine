b145166254e45428bc81f3052f42aa72cee678d6
Yankhoba <bagayoko.yan@gmail.com>
2023-12-07 10:57:04 +0100

    🚧 création dossier partials + navBar avec Bootstrap

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+include __DIR__. '/partials/header.php';
+include __DIR__. '/partials/nav.php';
+include __DIR__. '/partials/footer.php';
---
+++ partials/nav.php
@@ -0,0 +1,26 @@
+<nav class="navbar bg-body-tertiary">
+        <div class="container-fluid ">
+          <div class="logo"><img src="./src/assets/media/logo.jpg" alt="logo" class="imgLogo" width="50" height="40" /></div>
+          <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Catégories</a>
+            </li>
+
+          </ul>
+          <form class="d-flex" role="search">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Recherche
+            </button>
+          </form>
+        </div>
+      </nav>
+    </header>
