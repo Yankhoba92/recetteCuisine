2d08f4caa973ca224dd3a6e110375d35b53749a9
Yankhoba <bagayoko.yan@gmail.com>
2023-12-07 11:10:00 +0100

    🚧 création de searchForm & searchRecipe

---
+++ partials/searchForm.php
@@ -0,0 +1,11 @@
+<form class="d-flex" role="search" action="searchrecipe.php" method="Post">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Pasta, Chicken"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Recherche
+            </button>
+          </form>
